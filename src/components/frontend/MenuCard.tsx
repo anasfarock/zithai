a54114e783abaecbbf1687ae64319df1d038a317
anasfarock <anasf1905@gmail.com>
2025-09-15 12:01:10 +0500

    Updated Images and Layout for Cakes Sections

--- src/components/frontend/MenuCard.tsx
+++ src/components/frontend/MenuCard.tsx
@@ -96,27 +96,25 @@ export const Menu = () => {
                     {/* Cake Card 1 */}
                     <div className="bg-[#B8936C] rounded-3xl p-8 md:p-12 text-white relative overflow-visible">
                         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
-                            {/* Image column - positioned first for left overflow */}
+                            {/* Image column */}
                             <div className="relative lg:h-96 order-2 lg:order-1">
-                                {/* Mobile image - normal positioning */}
+                                {/* Mobile image */}
                                 <div className="lg:hidden relative h-64 w-full">
                                     <Image
                                         src="/images/cake-1.png"
                                         alt="Artisan Chocolate Cake"
                                         fill
-                                        className="object-cover rounded-2xl"
+                                        className="object-contain rounded-2xl"
                                     />
                                 </div>
-                                {/* Desktop image - centered overflow positioning */}
-                                <div className="hidden lg:block absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[32rem] h-96 z-20">
-                                    <div className="relative w-full h-full transform -translate-x-16">
-                                        <Image
-                                            src="/images/cake-1.png"
-                                            alt="Artisan Chocolate Cake"
-                                            fill
-                                            className="object-cover rounded-2xl"
-                                        />
-                                    </div>
+                                {/* Desktop image - centered */}
+                                <div className="hidden lg:block relative w-full h-full">
+                                    <Image
+                                        src="/images/cake-1.png"
+                                        alt="Artisan Chocolate Cake"
+                                        fill
+                                        className="object-contain rounded-2xl"
+                                    />
                                 </div>
                             </div>
                             {/* Text column */}
@@ -175,27 +173,25 @@ export const Menu = () => {
                                     </Button>
                                 </div>
                             </div>
-                            {/* Image column - positioned second for right overflow */}
-                            <div className="relative lg:h-96 order-2 overflow-visible">
-                                {/* Mobile image - normal positioning */}
+                            {/* Image column */}
+                            <div className="relative lg:h-96 order-2">
+                                {/* Mobile image */}
                                 <div className="lg:hidden relative h-64 w-full">
+                                    <Image
+                                        src="/images/cake-2.png"
+                                        alt="Vanilla Berry Cake"
+                                        fill
+                                        className="object-contain rounded-2xl"
+                                    />
+                                </div>
+                                {/* Desktop image - centered */}
+                                <div className="hidden lg:block relative w-full h-full">
                                     <Image
                                         src="/images/cake-2.png"
                                         alt="Vanilla Berry Cake"
                                         fill
                                         className="object-cover rounded-2xl"
                                     />
-                                </div>
-                                {/* Desktop image - overflow positioning extending beyond container */}
-                                <div className="hidden lg:block absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
-                                    <div className="relative w-[35rem] h-[35rem] transform translate-x-32">
-                                        <Image
-                                            src="/images/cake-2.png"
-                                            alt="Vanilla Berry Cake"
-                                            fill
-                                            className="object-contain"
-                                        />
-                                    </div>
                                 </div>
                             </div>
                         </div>
